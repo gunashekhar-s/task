43f817e19049ae9390f1374470661c579af10a2d
Gunashekhar <gunashekhars111@gmail.com>
2022-08-18 21:07:26 +0530

    created task2 file

--- task-2-updated.js
+++ task-2-updated.js
@@ -7,8 +7,7 @@ const triplets = (arr) => {
             for (let k = j + 1; k < arr.length; k++) {
                 const triplet = `${arr[i]}, ${arr[j]}, ${arr[k]}\n`
                 if (arr[i] + arr[j] + arr[k] === 0 && !uniqueTriplets.includes(triplet)) {
-                    uniqueTriplets.push(`${triplet}\n`)
-
+                    uniqueTriplets.push(triplet)
                 }
             }
         }
